7540a94b0397841f2ec891024fb3cfe814bf5b18
Daniel Koza <dakolech@gmail.com>
2017-08-30 16:19:40 +0200

    init repository

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module "api.middleware" {
+    import { Action } from 'redux';
+    export const API_CALL = "API_CALL";
+    export interface Config {
+        success: string;
+        failure: string;
+        type?: string;
+    }
+    export interface ApiCallAction extends Action {
+        payload: {
+            method: string;
+            path: string;
+            config: Config;
+            body?: any;
+            headers: any;
+        };
+    }
+    export interface Api {
+        get: (path: string, config: Config, headers?: any) => ApiCallAction;
+        post: (path: string, body: any, config: Config, headers?: any) => ApiCallAction;
+        put: (path: string, body: any, config: Config, headers?: any) => ApiCallAction;
+        patch: (path: string, body: any, config: Config, headers?: any) => ApiCallAction;
+        delete: (path: string, config: Config, headers?: any) => ApiCallAction;
+    }
+    export const api: Api;
+    export const apiMiddlewareCreator: any;
+}
